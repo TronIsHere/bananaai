a1e7c178f72a31f87c2de58e11358282ceeb79c0
Erwin <erwin0maleki@gmail.com>
2025-11-21 19:28:17 +0330

    added fav icons and seo configs

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,6 +76,26 @@ export const metadata: Metadata = {
     // google: "your-google-verification-code",
     // yandex: "your-yandex-verification-code",
   },
+  icons: {
+    icon: [
+      { url: "/icons/favicon.ico", sizes: "any" },
+      { url: "/icons/favicon.svg", type: "image/svg+xml" },
+      { url: "/icons/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+    ],
+    apple: [
+      {
+        url: "/icons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    other: [
+      {
+        rel: "manifest",
+        url: "/icons/site.webmanifest",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
